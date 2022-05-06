20459c4a259770aceaed76dbc2f010d22bf7590c
quyetnguyen02 <quyetnguyen0804@gmail.com>
2022-05-06 23:00:43 +0700

    push code back end client

---
+++ Data/MyDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using ClientNews.Models;
+
+namespace ClientNews.Data
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext() : base("name=CrawDB")
+        {
+        }
+
+        public DbSet<Sourses> Sourses { get; set; }
+        public DbSet<Article> Articles { get; set; }
+    }
+}
---
+++ Models/Sourses.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ClientNews.Models
+{
+    public class Sourses
+    {
+        public int Id { get; set; }
+        public string Url { get; set; }
+        public string SelectorSubUrl { get; set; }
+        public string SelectorTitle { get; set; }
+        public string SelectorImage { get; set; }
+        public string SelectorDescrition { get; set; }
+        public string SelectorContent { get; set; }
+        public string CategoryId { get; set; }
+    }
+}
